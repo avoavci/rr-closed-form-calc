c8795b05582c53d494407d98f0b309b93f74a389
avoavci <106642522+avoavci@users.noreply.github.com>
2022-06-03 18:30:17 +1000

    Fixed a bug!
    since if a0 = 1, the closed form = 1, I added a feature to make this automatically the case instead of providing an incorrect answer

--- closed form 1.py
+++ closed form 1.py
@@ -4,18 +4,21 @@ zan2 = input("input an2 ")
 a0 = int(za0)
 an1 = int(zan1)
 an2 = int(zan2)
-a1 = an1 * a0 + an2
-b0 = a1 - a0
-an3 = "x " + str(an1) + "^n"
-bn = b0 + an1
-an4 = an1 - 1
-an5 = (b0 / an4)
-an6 = str(an5)
-an7 = (an2 / an4)
-an8 = str(an7)
-if an7 >= 0:
-    an9 = " - "
+if a0 == 1: 
+    print("1")
 else:
-    an9 = " + "
-an10 = an6 + " " + an3 + an9 + an8
-print(an10)
+    a1 = an1 * a0 + an2
+    b0 = a1 - a0
+    an3 = "x " + str(an1) + "^n"
+    bn = b0 + an1
+    an4 = an1 - 1
+    an5 = (b0 / an4)
+    an6 = str(an5)
+    an7 = (an2 / an4)
+    an8 = str(an7)
+    if an7 >= 0:
+        an9 = " - "
+    else:
+        an9 = " + "
+    an10 = an6 + " " + an3 + an9 + an8
+    print(an10)
